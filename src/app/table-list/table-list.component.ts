8948535e3781eab81b48886489b1a2c4992cf5a0
Rajan Maghera <rmaghera@ualberta.ca>
2021-02-13 16:38:27 -0700

    Added tabbed page

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -13,12 +13,18 @@ import { catchError, retry } from 'rxjs/operators';
 export class TableListComponent implements OnInit {
 
   constructor( private http: HttpClient ) { }
-    Repdata;  
+    Repdata;
+    currentZone = "Edmonton zone"; 
   
   getEdmontonData() {    
     this.http.get('https://data.edmonton.ca/resource/2h73-35uw.json').subscribe(data => this.Repdata = data)
   }
 
+  setVariable(newVar) {
+    this.currentZone = newVar
+    this.ngOnInit()
+
+  }
 
   ngOnInit() {
     this.getEdmontonData()
